f1966795f0d223801455c07db4babb7311851842
Must4che <marek.tuska@accenture.com>
2019-03-12 13:34:18 +0100

    Patched process of creating path to JSON

--- bin/forcecalc.js
+++ bin/forcecalc.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 const forcecalc = require('../lib/app.js');
-const edit = require('../lib/edit.js');
+const { executeEdit } = require('../lib/edit.js');
 
 const args = process.argv.splice(process.execArgv.length + 2);
 
 if ( args[0] === '--edit') {
-    edit();
+    executeEdit();
 } else {
     forcecalc();
 }
