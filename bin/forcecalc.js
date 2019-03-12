c024d7a2a31e1a0cdce7d7b2bb40f8954ac55209
Must4che <marek.tuska116@gmail.com>
2019-03-12 15:58:43 +0100

    Added commands --version & --help

--- bin/forcecalc.js
+++ bin/forcecalc.js
@@ -2,11 +2,18 @@
 
 const forcecalc = require('../lib/app.js');
 const { executeEdit } = require('../lib/edit.js');
+const { help, VERSION_MESSAGE } = require('../lib/messages');
 
 const args = process.argv.splice(process.execArgv.length + 2);
 
-if ( args[0] === '--edit') {
-    executeEdit();
-} else {
+if ( args[0] === undefined ) {
     forcecalc();
+} else if ( args[0] === '--version' ) {
+    console.log(VERSION_MESSAGE);
+} else if ( args[0] === '--edit' ) {
+    executeEdit();
+} else if ( args[0] === '--help' ) {
+    help();
+} else {
+    help();
 }
